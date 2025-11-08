d09fc129c00327aaeb9efdf8548d0ac3f035bc8b
Lethios <204111230+Lethios@users.noreply.github.com>
2025-11-08 21:53:02 +0530

    Refactor part1 and add part2 function
    Refactor part1 function to accept input as a parameter and add part2 function to calculate the first basement entry index.

--- 2015/day01.rs
+++ 2015/day01.rs
@@ -1,9 +1,8 @@
 // https://adventofcode.com/2015/day/1
 
-fn part1() {
-    let input: &str = "<INPUT>";
+fn part1(input: &str) -> i32 {
     let mut floor: i32 = 0;
-    
+
     for chr in input.chars() {
         match chr {
             '(' => floor += 1,
@@ -11,6 +10,31 @@ fn part1() {
             _ => {}
         }
     }
-    
+
     floor
+}
+
+fn part2(input: &str) -> usize {
+    let mut floor: i32 = 0;
+
+    for (index, chr) in input.chars().enumerate() {
+        match chr {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => {}
+        }
+
+        if floor == -1 {
+            return index + 1;
+        }
+    }
+
+    0
+}
+
+fn main() {
+    let input: &str = "<INPUT>";
+
+    println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
 }
