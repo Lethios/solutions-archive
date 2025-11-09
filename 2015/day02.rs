c3578bd9e503992e8de1df96a80f77a7f18aa779
Lethios <204111230+Lethios@users.noreply.github.com>
2025-11-09 17:20:25 +0530

    Add surface area calculation in day02.rs
    Implement part1 function to calculate total surface area from input dimensions.

---
+++ 2015/day02.rs
@@ -0,0 +1,26 @@
+use std::io;
+use std::io::BufRead;
+
+fn part1() -> u32 {
+    let stdin = io::stdin();
+    let mut total_sum: u32 = 0;
+
+    for line in stdin.lock().lines() {
+        let expr: String = line.expect("Failed to read line");
+
+        let dimensions: Vec<u32> = expr
+            .split("x")
+            .map(|dim| dim.parse().expect("Failed to parse number"))
+            .collect();
+        
+        let (l, w, h) = (dimensions[0], dimensions[1], dimensions[2]);
+        let sur_area = (2 * l * w) + (2 * w * h) + (2 * h * l);
+        total_sum += sur_area;
+    }
+
+    total_sum
+}
+
+fn main() {
+    println!("Part 1: {}", part1())
+}
