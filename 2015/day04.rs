fe1b63d41a3f850545b035bf0e9aae6562ca216d
Lethios <204111230+Lethios@users.noreply.github.com>
2025-11-10 17:51:36 +0530

    Add part1 function for Advent of Code Day 4
    Implement part1 function to find the lowest number that produces a hash starting with '00000'.

--- 2015/day04.rs
+++ 2015/day04.rs
@@ -1,1 +1,30 @@
+// https://adventofcode.com/2015/day/4
 
+use md5::{Digest, Md5};
+
+fn part1(input: &str) -> u32 {
+    let mut number: u32 = 0;
+
+    let key: u32 = loop {
+        let mut hasher = Md5::new();
+
+        let payload: String = format!("{input}{number}");
+        hasher.update(payload.as_bytes());
+
+        let hash = hasher.finalize();
+        let hash_string: String = format!("{:x}", hash);
+
+        if &hash_string[..5] == "00000" {
+            break number;
+        }
+        number += 1;
+    };
+
+    key
+}
+
+fn main() {
+    let input: String = String::from("bgvyzdsv");
+
+    println!("Part 1: {}", part1(&input));
+}
