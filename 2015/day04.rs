1d6bc46e619236e6713a0b778197c132be9dc4fc
Lethios <204111230+Lethios@users.noreply.github.com>
2025-11-10 20:32:19 +0530

    Update day04.rs

--- 2015/day04.rs
+++ 2015/day04.rs
@@ -45,7 +45,7 @@ fn part2(input: &str) -> u32 {
 }
 
 fn main() {
-    let input: String = String::from("bgvyzdsv");
+    let input: String = String::from("<INPUT>");
 
     println!("Part 1: {}", part1(&input));
     println!("Part 2: {}", part2(&input));
