59a5eb5df252dab4395b6a56a9b6d43e6934bf28
Lethios <204111230+Lethios@users.noreply.github.com>
2025-11-26 15:22:00 +0530

    Create day02.rs

---
+++ 2017/day02.rs
@@ -0,0 +1,33 @@
+// https://adventofcode.com/2017/day/2
+
+use std::fs;
+
+fn part1(input: &str) -> u32 {
+    let mut checksum: u32 = 0;
+
+    for line in input.lines() {
+        let numbers: Vec<u32> = line
+            .split_whitespace()
+            .filter_map(|num| num.parse().ok())
+            .collect();
+
+        let largest: &u32 = numbers.iter().max().unwrap();
+        let smallest: &u32 = numbers.iter().min().unwrap();
+
+        checksum += (largest - smallest);
+    }
+
+    checksum
+}
+
+fn part2(input: &str) -> u32 {
+    0
+}
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Failed to read input file");
+
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
+}
+
