20374ebca7d6c2d94b922e0c9f3a446eba8f9e96
Lethios <204111230+Lethios@users.noreply.github.com>
2025-12-02 14:33:33 +0530

    Create day02.rs

---
+++ 2025/day02.rs
@@ -0,0 +1,41 @@
+// https://adventofcode.com/2025/day/2
+
+use std::fs;
+
+fn part1(input: &str) -> u64 {
+    let mut invalid_id_sum: u64 = 0;
+
+    let id_ranges: Vec<u64> = input
+        .trim()
+        .split([',', '-'])
+        .map(|num| num.parse().unwrap())
+        .collect();
+
+    for chunk in id_ranges.chunks(2) {
+        for num in chunk[0]..=chunk[1] {
+            let str_num: String = num.to_string();
+
+            if str_num.len() % 2 == 1 {
+                continue;
+            }
+
+            if &str_num[..(str_num.len() / 2)] == (&str_num[(str_num.len() / 2)..]) {
+                invalid_id_sum += num;
+            }
+        }
+    }
+
+    invalid_id_sum
+}
+
+fn part2(input: &str) -> u32 {
+    0
+}
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Failed to read input file");
+
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
+}
+
