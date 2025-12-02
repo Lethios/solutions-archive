e068af14119c110e5366fe30fae85d081a22e7f0
Lethios <204111230+Lethios@users.noreply.github.com>
2025-12-02 17:02:11 +0530

    Update part2.rs

--- 2025/Day 02/part2.rs
+++ 2025/Day 02/part2.rs
@@ -2,8 +2,36 @@
 
 use std::fs;
 
-fn part2(input: &str) -> u32 {
-    0
+fn part2(input: &str) -> u64 {
+    let mut invalid_id_sum: u64 = 0;
+
+    let id_ranges: Vec<u64> = input
+        .trim()
+        .split([',', '-'])
+        .map(|num| num.parse().unwrap())
+        .collect();
+
+    for chunk in id_ranges.chunks(2) {
+        for num in chunk[0]..=chunk[1] {
+            let str_num: String = num.to_string();
+
+            for pattern_len in 1..=str_num.len() / 2 {
+                if str_num.len() % pattern_len != 0 {
+                    continue;
+                }
+
+                let pattern: &str = &str_num[..pattern_len];
+                let pattern_check = pattern.repeat(str_num.len() / pattern_len);
+
+                if pattern_check == str_num {
+                    invalid_id_sum += num;
+                    break;
+                }
+            }
+        }
+    }
+
+    invalid_id_sum
 }
 
 fn main() {
