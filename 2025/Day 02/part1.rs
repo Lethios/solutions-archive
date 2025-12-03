5a5be405bdfb43173bd1b05634a7ccd9bb0465f0
Lethios <204111230+Lethios@users.noreply.github.com>
2025-12-03 09:59:56 +0530

    perf: improve runtime speed ~137ms -> ~31ms and readability

--- 2025/Day 02/part1.rs
+++ 2025/Day 02/part1.rs
@@ -12,14 +12,27 @@ fn part1(input: &str) -> u64 {
         .collect();
 
     for chunk in id_ranges.chunks(2) {
-        for num in chunk[0]..=chunk[1] {
-            let str_num: String = num.to_string();
+        let start: u64 = chunk[0];
+        let end: u64 = chunk[1];
 
-            if str_num.len() % 2 == 1 {
+        for num in start..=end {
+            let num_len = |mut num: u64| -> u64 {
+                let mut count = 0;
+
+                while num > 0 {
+                    num /= 10;
+                    count += 1;
+                }
+                count
+            };
+            let num_len: u64 = num_len(num);
+
+            if num_len % 2 == 1 {
                 continue;
             }
 
-            if &str_num[..(str_num.len() / 2)] == (&str_num[(str_num.len() / 2)..]) {
+            let half: u32 = (num_len / 2) as u32;
+            if num / 10u64.pow(half) == num % 10u64.pow(half) {
                 invalid_id_sum += num;
             }
         }
