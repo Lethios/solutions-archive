2359b087f83274d337611444902d4f45c85eba2f
Lethios <204111230+Lethios@users.noreply.github.com>
2025-12-04 11:35:59 +0530

    refactor: improve readability

--- 2025/Day 04/part1.rs
+++ 2025/Day 04/part1.rs
@@ -4,14 +4,18 @@ use std::fs;
 
 fn part1(input: &str) -> u32 {
     let mut accessible_rolls: u32 = 0;
-    let mut grid: Vec<Vec<char>> = Vec::new();
+
+    let mut grid: Vec<Vec<char>> = Vec::with_capacity(input.lines().count());
 
     for line in input.lines() {
         let row: Vec<char> = line.chars().collect();
         grid.push(row);
     }
 
-    let adjacent_tiles: [(isize, isize); 8] = [
+    let rows: usize = grid.len();
+    let cols: usize = grid[0].len();
+
+    const ADJACENT_TILES: [(isize, isize); 8] = [
         (-1, 1),
         (0, 1),
         (1, 1),
@@ -22,19 +26,19 @@ fn part1(input: &str) -> u32 {
         (1, -1),
     ];
 
-    for row in 0..grid.len() {
-        for col in 0..grid[0].len() {
+    for row in 0..rows {
+        for col in 0..cols {
             if grid[row][col] == '.' {
                 continue;
             }
 
             let mut neighbor_count = 0;
 
-            for (dx, dy) in adjacent_tiles {
+            for (dx, dy) in ADJACENT_TILES {
                 let x = row as isize + dx;
                 let y = col as isize + dy;
 
-                if x >= 0 && y >= 0 && (x as usize) < grid.len() && (y as usize) < grid[0].len() {
+                if x >= 0 && y >= 0 && (x as usize) < rows && (y as usize) < cols {
                     if grid[x as usize][y as usize] == '@' {
                         neighbor_count += 1;
                     }
