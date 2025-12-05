5dd9ebba54cc98bf225593f44fc9de9ce9ec2a60
Lethios <204111230+Lethios@users.noreply.github.com>
2025-12-05 10:59:07 +0530

    Update part1.rs

--- advent-of-code/2025/Day 05/part1.rs
+++ advent-of-code/2025/Day 05/part1.rs
@@ -1,1 +1,48 @@
+// https://adventofcode.com/2025/day/5
+
+use std::fs;
+
+fn part1(input: &str) -> u32 {
+    let mut fresh_ingredients: u32 = 0;
+
+    let mut id_ranges: Vec<Vec<u64>> = Vec::new();
+    let mut available_ingredients: Vec<u64> = Vec::new();
+    let mut available: bool = false;
+
+    for line in input.lines() {
+        if line.trim().is_empty() {
+            available = true;
+            continue;
+        }
+
+        if !available {
+            let id_range: Vec<u64> = line
+                .split("-")
+                .map(|range| range.parse().unwrap())
+                .collect();
+
+            id_ranges.push(id_range);
+        } else {
+            available_ingredients.push(line.parse().unwrap());
+        }
+    }
+
+    for ingredient in available_ingredients {
+        for range in &id_ranges {
+            if range[0] <= ingredient && ingredient <= range[1] {
+                fresh_ingredients += 1;
+                break;
+            }
+        }
+    }
+
+    fresh_ingredients
+}
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Failed to read input file");
+
+    println!("Part 1: {}", part1(&input));
+}
+
 
