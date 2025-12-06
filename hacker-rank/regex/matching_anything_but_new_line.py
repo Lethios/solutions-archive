5d4384a8b40630994d0719c90abf686322e93e03
Lethios <204111230+Lethios@users.noreply.github.com>
2025-12-06 12:42:41 +0530

    Update matching_anything_but_new_line.py

--- hacker-rank/regex/matching_anything_but_new_line.py
+++ hacker-rank/regex/matching_anything_but_new_line.py
@@ -1,6 +1,6 @@
 # https://www.hackerrank.com/challenges/matching-anything-but-new-line/problem
 
-regex_pattern = r'(.{3}\.){3}.{3}$'	# Do not delete 'r'.
+regex_pattern = r"(.{3}\.){3}.{3}$"	# Do not delete 'r'.
 
 import re
 import sys
