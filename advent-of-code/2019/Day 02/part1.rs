751f37d0b906de89e9c53845c319277081c7ce49
Lethios <204111230+Lethios@users.noreply.github.com>
2025-12-31 16:16:27 +0530

    refactor: use match for control flow

--- advent-of-code/2019/Day 02/part1.rs
+++ advent-of-code/2019/Day 02/part1.rs
@@ -3,28 +3,29 @@
 use std::fs;
 
 fn part1(input: &str) -> u32 {
-    let mut intcode: Vec<u32> = input.split(",").map(|val| val.parse().unwrap()).collect();
+    let mut intcode: Vec<u32> = input.trim().split(",").map(|val| val.parse().unwrap()).collect();
 
     intcode[1] = 12;
     intcode[2] = 2;
 
     for idx in (0..intcode.len()).step_by(4) {
-        if intcode[idx] == 1 {
-            let a_idx: usize = intcode[idx + 1] as usize;
-            let b_idx: usize = intcode[idx + 2] as usize;
-            let output_idx: usize = intcode[idx + 3] as usize;
+        match intcode[idx] {
+            1 => {
+                let a_idx: usize = intcode[idx + 1] as usize;
+                let b_idx: usize = intcode[idx + 2] as usize;
+                let output_idx: usize = intcode[idx + 3] as usize;
 
-            intcode[output_idx] = intcode[a_idx] + intcode[b_idx];
+                intcode[output_idx] = intcode[a_idx] + intcode[b_idx];
+            },
+            2 => {
+                let a_idx: usize = intcode[idx + 1] as usize;
+                let b_idx: usize = intcode[idx + 2] as usize;
+                let output_idx: usize = intcode[idx + 3] as usize;
 
-        } else if intcode[idx] == 2 {
-            let a_idx: usize = intcode[idx + 1] as usize;
-            let b_idx: usize = intcode[idx + 2] as usize;
-            let output_idx: usize = intcode[idx + 3] as usize;
-
-            intcode[output_idx] = intcode[a_idx] * intcode[b_idx];
-            
-        } else {
-            break;
+                intcode[output_idx] = intcode[a_idx] * intcode[b_idx];
+            },
+            99 => break,
+            _ => {}
         }
     }
     
