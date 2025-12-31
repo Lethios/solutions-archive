6fff965016c6087c7b9cad99251f511f90b064bc
Lethios <204111230+Lethios@users.noreply.github.com>
2025-12-31 16:11:56 +0530

    Update part1.rs

--- advent-of-code/2019/Day 02/part1.rs
+++ advent-of-code/2019/Day 02/part1.rs
@@ -1,1 +1,38 @@
+// https://adventofcode.com/2019/day/2
 
+use std::fs;
+
+fn part1(input: &str) -> u32 {
+    let mut intcode: Vec<u32> = input.split(",").map(|val| val.parse().unwrap()).collect();
+
+    intcode[1] = 12;
+    intcode[2] = 2;
+
+    for idx in (0..intcode.len()).step_by(4) {
+        if intcode[idx] == 1 {
+            let a_idx: usize = intcode[idx + 1] as usize;
+            let b_idx: usize = intcode[idx + 2] as usize;
+            let output_idx: usize = intcode[idx + 3] as usize;
+
+            intcode[output_idx] = intcode[a_idx] + intcode[b_idx];
+
+        } else if intcode[idx] == 2 {
+            let a_idx: usize = intcode[idx + 1] as usize;
+            let b_idx: usize = intcode[idx + 2] as usize;
+            let output_idx: usize = intcode[idx + 3] as usize;
+
+            intcode[output_idx] = intcode[a_idx] * intcode[b_idx];
+            
+        } else {
+            break;
+        }
+    }
+    
+    intcode[0]
+}
+
+fn main() {
+    let input: String = fs::read_to_string("input.txt").expect("Failed to read input file");
+
+    println!("Part 1: {}", part1(&input));
+}
