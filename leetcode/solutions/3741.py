d47de2def24eceb1ff0d258529bfbcfd64f19ec3
Lethios <204111230+Lethios@users.noreply.github.com>
2026-04-13 13:18:41 +0530

    Create 3741.py

---
+++ leetcode/solutions/3741.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/minimum-distance-between-three-equal-elements-ii/
+
+class Solution:
+    def minimumDistance(self, nums: List[int]) -> int:
+        arr = [[] for _ in range(len(nums) + 1)]
+        min_dist = 10**8
+
+        for i, num in enumerate(nums):
+            arr[num].append(i)
+
+            if len(arr[num]) > 3:
+                arr[num].pop(0)
+
+            if len(arr[num]) == 3:
+                min_dist = min(min_dist, 2 * (arr[num][2] - arr[num][0]))
+
+        return min_dist if min_dist != 10**8 else -1
