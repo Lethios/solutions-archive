e804bf1a6a4f6bf99b6523129a1fb32ba030292d
Lethios <204111230+Lethios@users.noreply.github.com>
2026-04-14 11:36:08 +0530

    Create 0023.py

---
+++ leetcode/solutions/0023.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+import heapq
+
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        min_heap = []
+
+        for node in lists:
+            while node:
+                heapq.heappush(min_heap, node.val)
+                node = node.next
+
+        if not min_heap:
+            return None
+
+        head = ListNode(heapq.heappop(min_heap))
+        curr_node = head
+
+        while min_heap:
+            curr_node.next = ListNode(heapq.heappop(min_heap))
+            curr_node = curr_node.next
+
+        return head
