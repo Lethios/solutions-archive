6529c41168255108f5b433b230cfb93921b9edaa
Lethios <204111230+Lethios@users.noreply.github.com>
2026-06-13 12:54:11 +0530

    Update part2.rs

--- advent-of-code/2023/Day 01/part2.rs
+++ advent-of-code/2023/Day 01/part2.rs
@@ -3,7 +3,43 @@
 use std::fs;
 
 fn part2(input: &str) -> u32 {
-    0
+    let mut calibration_sum: u32 = 0;
+
+    let words: [(&str, u32); 9] = [
+    ("one", 1),
+    ("two", 2),
+    ("three", 3),
+    ("four", 4),
+    ("five", 5),
+    ("six", 6),
+    ("seven", 7),
+    ("eight", 8),
+    ("nine", 9),
+];
+
+    for line in input.lines() {
+        let mut digits: Vec<u32> = Vec::new();
+
+        for (idx, chr) in line.chars().enumerate() {
+            if chr.to_digit(10).is_some() {
+                digits.push(chr.to_digit(10).unwrap());
+            }
+
+            let check: &str = &line[idx..];
+
+            for (word, num) in words {
+                if check.starts_with(word) {
+                    digits.push(num);
+                    break;
+                }
+            }
+
+        }
+
+        calibration_sum += digits[0] * 10 + digits.last().unwrap();
+    }
+
+    calibration_sum
 }
 
 fn main() {
