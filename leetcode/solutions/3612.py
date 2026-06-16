f4df1b954448973220857b344272884682ec3bcc
Lethios <204111230+Lethios@users.noreply.github.com>
2026-06-16 10:22:50 +0530

    Create 3612.py

---
+++ leetcode/solutions/3612.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/process-string-with-special-operations-i/
+
+class Solution:
+    def processStr(self, s: str) -> str:
+        res = ""
+
+        for char in s:
+            match char:
+                case "*":
+                    res = res[:-1]
+                case "#":
+                    res += res
+                case "%":
+                    res = res[::-1]
+                case _:
+                    res += char
+
+        return res
