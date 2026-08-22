6f70f031965b9adcb83798457f350276800cd9d0
Lethios <204111230+Lethios@users.noreply.github.com>
2026-08-22 18:38:53 +0530

    Create 3622.rs

---
+++ leetcode/solutions/3622.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn check_divisibility(n: i32) -> bool {
+        let mut temp = n;
+        let mut sum = 0;
+        let mut product = 1;
+
+        while temp > 0 {
+            let digit = temp % 10;
+            sum += digit;
+            product *= digit;
+            temp /= 10;
+        }
+
+        n % (sum + product) == 0
+    }
+}
