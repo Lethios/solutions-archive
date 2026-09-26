0c166b82b3a2d673d04b3da8b4b8ca5bc2120c11
Lethios <204111230+Lethios@users.noreply.github.com>
2026-09-26 12:19:39 +0530

    Update part1.rs

--- advent-of-code/2025/Day 07/part1.rs
+++ advent-of-code/2025/Day 07/part1.rs
@@ -1,50 +1,36 @@
 // https://adventofcode.com/2025/day/7
 
-use std::{
-    collections::{HashSet, VecDeque},
-    fs,
-};
+use std::fs;
 
-fn part1(input: &str) -> u32 {
-    let mut beam_splits: u32 = 0;
+fn part1(input: &str) -> i32 {
+    let mut loc = [false; 142];
+    let mut res = 0;
 
-    let mut splitters: Vec<(usize, usize)> = Vec::new();
-    let mut tachyon_beams: VecDeque<(usize, usize)> = VecDeque::new();
-    let mut hashset: HashSet<(usize, usize)> = HashSet::new();
+    for line in input.lines() {
+        for (i, char) in line.chars().enumerate() {
+            if char == 'S' {
+                loc[i] = true;
+            } else if char == '^' && loc[i] {
+                loc[i] = false;
 
-    for (row, line) in input.lines().enumerate() {
-        for (col, chr) in line.chars().enumerate() {
-            match chr {
-                'S' => tachyon_beams.push_back((row, col)),
-                '^' => splitters.push((row, col)),
-                _ => {}
+                if i > 0 {
+                    loc[i - 1] = true;
+                }
+
+                if i + 1 < loc.len() {
+                    loc[i + 1] = true;
+                }
+
+                res += 1;
             }
         }
     }
 
-    while tachyon_beams.len() > 0 {
-        let beam = tachyon_beams.pop_front().unwrap();
-
-        if hashset.contains(&beam) {
-            continue;
-        } else {
-            hashset.insert(beam);
-            beam_splits += 1;
-        }
-
-        for splitter in &splitters {
-            if splitter.1 == beam.1 && splitter.0 > beam.0 {
-                tachyon_beams.push_back((splitter.0, splitter.1 - 1));
-                tachyon_beams.push_back((splitter.0, splitter.1 + 1));
-            }
-        }
-    }
-
-    beam_splits
+    res
 }
 
 fn main() {
-    let input = fs::read_to_string("input.txt").expect("Failed to read input file");
+    let input: String = fs::read_to_string("input.txt").expect("Failed to read input file");
 
     println!("Part 1: {}", part1(&input));
 }
